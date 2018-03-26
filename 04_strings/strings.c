0bc7519e6af0a53a8da32455431596de23075221
Jeluchu <jesusmarcalderon@gmail.com>
2018-03-26 14:10:26 +0200

    STRINGS

---
+++ 04_strings/strings.c
@@ -0,0 +1,35 @@
+//PROGRAMA EN C++
+//AUTOR: JESÚS MARÍA CALDERÓN - GITHUB JELUCHU
+//https://github.com/Jeluchu
+
+#include <stdio.h>
+
+int main(){
+
+   char ch,nch;	
+   int count;	
+   int k;	
+
+   printf("Introduce una '#' al final del string: ");
+   scanf("%c",&ch);	
+   while(ch != '#'){
+	if(ch >= '0' && ch <= '9'){
+
+	  count = ch-'0'+1;	
+	  scanf("%c",&nch);	
+
+	  for(k=0;k<count;k++)	
+	     printf("%c",nch);
+
+	}else
+
+	  printf("%c",ch);	
+	  printf("-");			
+	  scanf("%c",&ch);		
+	}
+
+   printf("#\n");	
+
+   return 0;			
+}
+
